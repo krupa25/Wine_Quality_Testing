5efcdffcf48bb30e73ac25bf98945be70831bba3
Dipak Rathod <dipakrathod258@gmail.com>
2020-02-27 16:21:27 +0530

    Home

--- wine_quality_testing/views.py
+++ wine_quality_testing/views.py
@@ -8,6 +8,21 @@ def home(request):
 
 def wineDetails(request):
     wine1 = wine()
+    
+    alochol = request.POST.get("alcohol")
+    malic_acid = request.POST.get("malic_acid")
+    ash = request.POST.get("ash")
+    alcalinity_of_ash = request.POST.get("alcalinity_of_ash")
+    magnesium = request.POST.get("magnesium")
+    total_phenols = request.POST.get("total_phenols")
+    flavanoids = request.POST.get("flavanoids")
+    nonflavanoid_phenols = request.POST.get("nonflavanoid_phenols")
+    proanthocyanins =request.POST.get("proanthocyanins")
+    color_intensity = request.POST.get("color_intensity")
+    hue = request.POST.get("hue")
+    od315_of_diluted_wines = request.POST.get("od315_of_diluted_wines")
+    proline = request.POST.get("proline")
+
     wine1.alcohol = request.POST.get("alcohol")
     wine1.malic_acid = request.POST.get("malic_acid")
     wine1.ash = request.POST.get("ash")
@@ -21,30 +36,30 @@ def wineDetails(request):
     wine1.hue = request.POST.get("hue")
     wine1.od315_of_diluted_wines = request.POST.get("od315_of_diluted_wines")
     wine1.proline = request.POST.get("proline")
-    print(wine1.save())
+
+
+    X = [[alochol, malic_acid, ash, alcalinity_of_ash, magnesium, total_phenols, flavanoids, nonflavanoid_phenols, proanthocyanins, color_intensity, hue, od315_of_diluted_wines, proline ]]
+
+    loaded_model = joblib.load('/home/dipak/workspace/Wine_Quality_Testing/ML/final_prediction.pickle')
     
-    id = wine.objects.latest('id')
-
-    print(id)
-
-    # return redirect("/modelPrediction/2")
-
-    response = HttpResponse("Success")
+    score = loaded_model.predict(X)
+    print(score)
+    response = HttpResponse("Ho gaya")
     return response
 
 def modelPrediction(request ,id):
     print("=====start===")
     X = list(wine.objects.filter(id=id))
-
-    print(id)
-    print(type(X))
-    print("=====end===")
-    for x in X:
-        print(x)
-    # =======Linking ML Model begins====
-    loaded_model = joblib.load('/home/volv/wine_quality/ML/final_prediction.pickle')
+    pass
+    # print(id)
+    # print(type(X))
+    # print("=====end===")
+    # for x in X:
+    #     print(x)
+    # # =======Linking ML Model begins====
+    # loaded_model = joblib.load('/home/volv/wine_quality/ML/final_prediction.pickle')
     
-    score = loaded_model.predict(X)
-    # =======Linking ML Model ends====
+    # score = loaded_model.predict(X)
+    # # =======Linking ML Model ends====
 
     return render(request, "prediction.html", {"score": score})
